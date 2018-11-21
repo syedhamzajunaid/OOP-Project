3db601c872501c2e87fb56002889381ffbc5d0f8
syedhamzajunaid <34619883+syedhamzajunaid@users.noreply.github.com>
2018-11-21 22:45:47 +0500

    Vehicles base class

--- Vehicles.h
+++ Vehicles.h
@@ -4,15 +4,17 @@
 class Vehicles: public GameObject
 {
 protected:
-    int speed_x, speed_y ;
-    float friction ;
+    int speed_x, speed_y ; // variables to control the motion of the vehicle.
+    float friction ; // variable to smoothen the animation.
     double angle ;
 public:
+    //---Construtor and Destructor.
     Vehicles() {};
     ~Vehicles()
     {
         std::cout << "Vehicle destroyed." << std::endl ;
     } ;
+    //---Pure Virtual Functions.
     virtual void Render() = 0 ;
     virtual void Move() = 0 ;
     virtual void Update() = 0 ;
