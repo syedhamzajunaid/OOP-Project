20ea1f5fd09652ac19576a92d9ff466a7186836d
syedhamzajunaid <34619883+syedhamzajunaid@users.noreply.github.com>
2018-11-21 22:10:29 +0500

    Add files via upload
    Vehicles base class.

---
+++ Vehicles.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Vehicles: public GameObject
+{
+private:
+    int speed_x, speed_y ;
+    float friction ;
+    double angle ;
+    SDL_Rect vehicle ;
+public:
+    Vehicles() {};
+    ~Vehicles()
+    {
+        std::cout << "Vehicle destroyed." << std::endl ;
+    } ;
+    virtual void Render() = 0 ;
+    virtual void Move() = 0 ;
+};
